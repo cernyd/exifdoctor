ec6c361a676d844dc8f5fa367674883f098e1a60
David Černý <18537381+cernyd@users.noreply.github.com>
2025-04-18 20:26:34 +0200

    add exif save

--- src/exifdoctor/exiftool_wrapper.py
+++ src/exifdoctor/exiftool_wrapper.py
@@ -4,6 +4,23 @@ import subprocess
 
 
 def load_exif(file_path: Path):
-    result = subprocess.run(["exiftool", "-n", "-j", str(file_path.resolve())],
-                            capture_output=True, text=True)
+    result = subprocess.run(
+        ["exiftool", "-n", "-j", str(file_path.resolve())],
+        capture_output=True, text=True
+    )
     return json.loads(result.stdout)
+
+
+def save_exif(file_path: Path, edits: dict[str, str]):
+    command = ["exiftool"]
+    for key, value in edits.items():
+        command.append(f'-{key}="{value}"')
+
+    command.append(str(file_path.resolve()))
+
+    print(command)
+    # TODO: dry run for now
+    return
+    result = subprocess.run(command, capture_output=True, text=True)
+    print(result.stdout)
+    print(result.stderr)
